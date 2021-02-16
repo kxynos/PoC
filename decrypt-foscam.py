1ad2fb2ac9a0590b1fdb97b189be1746a5924b9d
Konstantinos Xynos <kxynos@users.noreply.github.com>
2021-02-16 13:33:11 +0100

    Update decrypt-foscam.py
    File check before removal

--- decrypt-foscam.py
+++ decrypt-foscam.py
@@ -202,5 +202,5 @@ if __name__ == "__main__":
 					if (os.path.isfile(outfile)):
 						os.remove(outfile)
 	print "Cleaning up..."
-	os.remove(outfile)
+	if (os.path.isfile(outfile)): os.remove(outfile)
 	sys.exit(1)
